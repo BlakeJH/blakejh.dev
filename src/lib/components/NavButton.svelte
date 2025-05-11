ec5d8eb817d52861725d98b33560e6590ac82097
BlakeJH <blakejenkinshanks@gmail.com>
2025-05-10 22:56:51 -0600

    Initial commit

---
+++ src/lib/components/NavButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+    import {goto} from "$app/navigation";
+    export let icon;
+    export let page: string;
+
+    function handleClick() {
+        goto('/' + page);
+    }
+</script>
+
+<div
+        on:click={handleClick}
+        class="transition-transform hover:-translate-y-2 duration-300 cursor-pointer"
+>
+    <Icon icon={icon} width={50} height={50} />
+</div>
+
+
+
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,42 @@
+<script>
+    import NavButton from "$lib/components/NavButton.svelte";
+</script>
+
+<div class="navbar">
+    <div class="nav-inner w-[730px] h-[60px] bg-neutral-900 rounded-[20px]">
+        <div class="icon">
+            <NavButton icon="ic:baseline-home" page="urmom" />
+            <NavButton icon="ic:baseline-settings" page="urmom" />
+            <NavButton icon="ic:baseline-settings" page="urmom" />
+            <NavButton icon="ic:baseline-settings" page="urmom" />
+            <NavButton icon="ic:baseline-settings" page="urmom" />
+        </div>
+    </div>
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 15px;
+    }
+
+    .nav-inner {
+        background-color: #111111;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 60px;
+    }
+
+    .icon {
+        color: white;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 50px;
+    }
+</style>
